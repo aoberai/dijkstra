a3d5bb51729931c5e10aa97694ded623798ef4e9
Aditya-Paly <aoberai05@gmail.com>
2020-03-16 11:33:59 -0700

    code works for first iteration where distance to source vertwex is 0

--- src/Graph.java
+++ src/Graph.java
@@ -3,35 +3,57 @@ import java.util.*;
 public class Graph {
 
     private static ArrayList<Edge> graph = new ArrayList<>();
-    private static int numOfVertices = 5;
+    private static int numOfVertices = 4;
 
     public static void main(String[] args) {
+        graph.add(new Edge(1, 2, 1));
+        graph.add(new Edge(2, 3, 1));
+        graph.add(new Edge(3, 4, 1));
         graph.add(new Edge(1, 2, 3));
+        graph.add(new Edge(3, 4, 2));
+        graph.add(new Edge(1, 4, 3));
+        graph.toArray();
         System.out.println(shortestSubPathInPath());
     }
 
     public static int shortestSubPathInPath() {
-        HashSet<Integer> settled = new HashSet<>();
-        PriorityQueue<Edge> distanceFinder = new PriorityQueue<>();
-        int source = 0;
-        int currentNode = source;
-        graph.add(new Edge(0,0,0));
-        int[] distances = new int[numOfVertices];
-        for (Edge e : graph) {
-            if (e.node1 != currentNode) {
-                distances[e.node2] = Integer.MAX_VALUE;
+        ArrayList<Boolean> settled = new ArrayList<>();
+        PriorityQueue<Edge> distanceFinder = new PriorityQueue<>(new Comparator<Edge>() {
+            @Override
+            public int compare(Edge o1, Edge o2) {
+                return o1.cost-o2.cost;
             }
-            distanceFinder.add(e);
+        });
+        int currentNode = 1;
+        int[] distances = new int[numOfVertices];
+        graph.add(new Edge(1,1,0));
+        distanceFinder.addAll(graph);
+        for (int i = 0; i < numOfVertices; i++) {
+            distances[i] = Integer.MAX_VALUE;
+            settled.add(false);
         }
-        distances[source] = 0;
+        distanceFinder.size();
+//        distances[0] = 0;
         while (distanceFinder.size() != 0) {
             Edge e = distanceFinder.poll();
-            distances[e.node2] = e.cost;
+            System.out.println(e);
+            distances[e.node2 - 1] = e.cost;
+            Iterator iter = distanceFinder.iterator();
+            settled.set(e.node2 - 1, true);
+//            while (iter.hasNext()) {
+//                Edge nextEdge = (Edge) iter.next();
+//                if (nextEdge.node1 == e.node2) {
+//                    int a = nextEdge.cost + distances[nextEdge.node2];
+//                    if (a < distances[nextEdge.node2]) {
+//                        distances[nextEdge.node2] = a;
+//                    }
+//                }
+//            }
         }
-        return distances[1];
+        return distances[3];
     }
 
-    public static class Edge implements Comparator<Edge> {
+    public static class Edge {
         private int node1, node2, cost;
         Edge (int node1, int node2, int cost) {
             this.node1 = node1;
@@ -52,8 +74,12 @@ public class Graph {
         }
 
         @Override
-        public int compare(Edge o1, Edge o2) {
-            return o1.cost - o2.cost;
+        public String toString() {
+            return "Edge{" +
+                    "node1=" + node1 +
+                    ", node2=" + node2 +
+                    ", cost=" + cost +
+                    '}';
         }
     }
 }
